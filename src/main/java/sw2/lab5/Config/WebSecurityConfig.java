67b4e34abbe2bb3b0e81219a07126ea6701bdd99
ruthnadiezhda <a20150875@pucp.edu.pe>
2020-05-18 19:34:23 -0500

    Ejercicio 1

--- src/main/java/sw2/lab5/Config/WebSecurityConfig.java
+++ src/main/java/sw2/lab5/Config/WebSecurityConfig.java
@@ -20,14 +20,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception  {
         http.formLogin();
         http.authorizeRequests()
+            .antMatchers("/post/**").authenticated()
+            .antMatchers("/user/**").authenticated()
             .anyRequest().permitAll();
+
+        http.authorizeRequests()
+                .antMatchers("/user/list").hasAuthority("admin")
+                .antMatchers("/user","/user/edit").hasAnyAuthority("user","admin")
+                .antMatchers("/post/delete").hasAuthority("admin")
+                .antMatchers("/post/create","/post/edit").hasAnyAuthority("user","admin")
+                .anyRequest().permitAll();
+
     }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
-            .dataSource(dataSource)
-            .passwordEncoder(new BCryptPasswordEncoder());
+                .dataSource(dataSource)
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .usersByUsernameQuery("SELECT email, password, active FROM lab5.user WHERE email=? ")
+                .authoritiesByUsernameQuery("SELECT u.email , r.role_name  FROM lab5.user u, lab5.role r WHERE u.id_role=r.id_role");
     }
 
 
